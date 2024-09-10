17f29abb3dbbe874e389fabc54c474c2c0b10f36
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-09-10 21:50:25 +0100

    UART not working yet

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 ## LED
 The LED display is acces via the GPIO pins.
-- GPIO_BASE = 0x50000000
-- P0 = 0x50000000
-- P1 = 0x50000300
+- P0 GPIO_BASE = 0x50000000
+- P1 GPIO_BASE = 0x50000300
 - P0 covers P0.00 to P0.31
 - P0.15 = row3
 - P0.31 = col3
 
+## UART
+"A UART transmission sequence is started by triggering the STARTTX task.
 
+Bytes are transmitted by writing to the TXD register. When a byte has been successfully transmitted, the UART will generate a TXDRDY event after which a new byte can be written to the TXD register. A UART transmission sequence is stopped immediately by triggering the STOPTX task."
+from https://docs.nordicsemi.com/bundle/ps_nrf52833/page/uart.html
+
+- P0.06 = UART_INT_RX
+- P1.08 = UART_INT_TX
--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,39 @@
 
 extern crate panic_halt;
 
-use core::ptr::write_volatile;
+use core::ptr::{read_volatile, write_volatile};
 // use microbit::{board::Board, hal::prelude::OutputPin};
 
-const GPIO_BASE_VAL: u32 = 0x50000000;
-const _GPIO_BASE: *mut u32 = GPIO_BASE_VAL as *mut u32;
-const DIRSET_OFFSET: *mut u32 = (GPIO_BASE_VAL + 0x518) as *mut u32;
-const OUTSET_OFFSET: *mut u32 = (GPIO_BASE_VAL + 0x508) as *mut u32;
-const OUTCLR_OFFSET: *mut u32 = (GPIO_BASE_VAL + 0x50C) as *mut u32;
+// GPIO
+const GPIO_BASE_P0: u32 = 0x50000000;
+const GPIO_BASE_P1: u32 = 0x50000300;
+const DIRSET_P0: *mut u32 = (GPIO_BASE_P0 + 0x518) as *mut u32;
+const DIRSET_P1: *mut u32 = (GPIO_BASE_P1 + 0x518) as *mut u32;
+const OUTSET_P0: *mut u32 = (GPIO_BASE_P0 + 0x508) as *mut u32;
+const OUTCLR_P0: *mut u32 = (GPIO_BASE_P0 + 0x50C) as *mut u32;
+
+// UART
+const UART_BASE: u32 = 0x40002000;
+const UART_ENABLE: *mut u32 = (UART_BASE + 0x500) as *mut u32;
+const UART_PINSELTXD: *mut u32 = (UART_BASE + 0x50C) as *mut u32;
+const UART_STARTTX: *mut u32 = (UART_BASE + 0x008) as *mut u32;
+const UART_TXD: *mut u32 = (UART_BASE + 0x51C) as *mut u32;
+const UART_TXREADY: *mut u32 = (UART_BASE + 0x11C) as *mut u32;
+const BAUD: *mut u32 = (UART_BASE + 0x524) as *mut u32;
 
 // https://tech.microbit.org/hardware/schematic/
 const ROW1: u32 = 21;
 const COL1: u32 = 28;
 const _ROW3: u32 = 15;
 const _COL3: u32 = 31;
+
+fn uart_send(x: u32) {
+    unsafe {
+        while read_volatile(UART_TXREADY) == 0 {}
+        write_volatile(UART_TXREADY, 0);
+        write_volatile(UART_TXD, x);
+    }
+}
 
 // The `#[entry]` attribute macro is provided by the `cortex_m_rt` crate as a program entry point *after* static variable have been initialised.
 #[cortex_m_rt::entry]
@@ -34,11 +53,21 @@ fn entry_point() -> ! {
     // board.display_pins.col3.set_low().unwrap();
     // board.display_pins.row3.set_high().unwrap();
     unsafe {
-        write_volatile(DIRSET_OFFSET, 1 << ROW1);
-        write_volatile(DIRSET_OFFSET, 1 << COL1);
+        write_volatile(DIRSET_P0, 1 << ROW1);
+        write_volatile(DIRSET_P0, 1 << COL1);
 
-        write_volatile(OUTCLR_OFFSET, 1 << COL1);
-        write_volatile(OUTSET_OFFSET, 1 << ROW1);
+        write_volatile(OUTCLR_P0, 1 << COL1);
+        write_volatile(OUTSET_P0, 1 << ROW1);
+
+        // UART
+        write_volatile(DIRSET_P1, 1 << 8);
+        write_volatile(BAUD, 0x01D7E000);
+        write_volatile(UART_PINSELTXD, 0x28);
+        write_volatile(UART_ENABLE, 4);
+        write_volatile(UART_STARTTX, 1);
+        write_volatile(UART_TXD, 0x00);
     }
-    loop {}
+    loop {
+        uart_send(0x30 + (1 & 7));
+    }
 }
