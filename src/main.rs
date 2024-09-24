7d01e5a8d3ed45b2a52c2737f52017200f7beac3
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-09-24 19:17:03 +0100

    improve comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![no_main]
 #![no_std]
-
-use core::arch::global_asm;
+// 'binary' crates in Rust must include a function called 'main' to define the executable entry point unless the `no_main` attribute may be applied at the crate level.
+#![no_main]
 
 use compiling::entry_point;
+use core::arch::global_asm;
 
+// #[panic_handler] is used to define the behavior of the Rust `panic!` macro (a panic is a fatal exception) in #![no_std] applications.
+// https://doc.rust-lang.org/nomicon/panic-handler.html
 #[panic_handler]
 fn panic_handler(_: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -21,7 +23,7 @@ global_asm!(
 
 #[export_name = "Reset_Handler"]
 pub unsafe extern "C" fn reset() {
-    entry_point()
+    entry_point();
 }
 
 #[link_section = ".vectors.reset_handler"]
